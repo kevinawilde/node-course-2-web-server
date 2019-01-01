61cf971f50355fa0e5ff5d70b2533e98d48a42bc
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-01-01 13:12:36 -0600

    heroku added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -18,9 +20,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use((req, res, next) =>{
+/*app.use((req, res, next) =>{
 	res.render('maintenence.hbs')
-});
+});*/
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () =>{
@@ -50,6 +52,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is listening on port 3000');
+app.listen(port, () => {
+	console.log(`Server is listening on ${port}`);
 });
