f7ba6aaa750f4ba4748013b753fb6aa44f82e442
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-01-01 13:28:49 -0600

    heroku new feature test

--- server.js
+++ server.js
@@ -40,6 +40,13 @@ app.get('/', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		welcomeMessage: 'Welcome the Projects Section!'
+	});
+});
+
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
 		pageTitle: 'About Page'
